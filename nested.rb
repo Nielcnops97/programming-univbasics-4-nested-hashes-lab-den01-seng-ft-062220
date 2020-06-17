010308e3b4657e7815318df703f829fb1cfcd99b
Niel Cnops <nielcnops@outlook.com>
2020-06-17 16:05:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 #return adding_matz[]
 
 	programmer_hash = {
-	  :yukihiro_matsumoto => {
+	  :yukihiro_matsumoto => 
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
