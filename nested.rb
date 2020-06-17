0014d09796c7716e5adb9c70fef612347ba5e2f3
Niel Cnops <nielcnops@outlook.com>
2020-06-17 16:01:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,11 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- :yukihiro_matsumoto  {
-  :known_for => "Ruby",
-   :languages => ["LISP", "C"] }
-return adding_matz[]
-end
+ #:yukihiro_matsumoto  =>{
+  #:known_for => "Ruby",
+   #:languages => ["LISP", "C"] }
+#return adding_matz[]
+
 	programmer_hash = {
       :grace_hopper => {
         :known_for => "COBOL",
